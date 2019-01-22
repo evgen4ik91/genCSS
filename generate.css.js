867153fecb78d733c4b65a492de215fcc57647a1
yauheni_chyrkouski <evgen.box@live.ru>
2019-01-22 12:22:00 +0300

    args parser added

--- generate.css.js
+++ generate.css.js
@@ -1,4 +1,11 @@
+const program = require('commander');
+
+program
+  .version('0.1.0')
+  .option('-p, --filepath [value]', 'Type path')
+  .option('-n, --filename [value]', 'Type name')
+  .parse(process.argv);
+
 console.log('run');
-process.argv.forEach(function (val, index, array) {
-    console.log(index + ': ' + val);
-});
+if (program.filepath) console.log(`${program.filepath} - filepath`);
+if (program.filename) console.log(`${program.filename} - filename`);
