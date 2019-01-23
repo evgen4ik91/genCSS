82a844afb915352f62c651f2e321e37bbb0defcf
yauheni_chyrkouski <evgen.box@live.ru>
2019-01-23 11:08:41 +0300

    file write

--- generate.css.js
+++ generate.css.js
@@ -1,5 +1,11 @@
+'use strict';
+
+const fs = require('fs');
 const program = require('commander');
 const prompt = require('prompt-sync')();
+
+const times = 1000000;
+const scriptFileName = __filename.split(/[\\/]/).pop();
 
 program
   .version('0.1.0')
@@ -7,7 +13,41 @@ program
   .option('-n, --filename [value]', 'Type name')
   .parse(process.argv);
 
-let filepath = program.filepath ? program.filepath : prompt('Path is required. Type the filepath: ');
 
-console.log(`${filepath} - filepath`);
-if (program.filename) console.log(`${program.filename} - filename`);
+let loading = {
+    frames: ['.', '..', '...'],
+    interval: null,
+    start() {
+        let i = 0;
+        let framesLength = this.frames.length - 1;
+        this.interval = setInterval(() => {
+            i = (i > framesLength) ? 0 : i;
+            console.clear();
+            console.log(this.frames[i]);
+            i++;
+        }, 300);
+    },
+    stop() {
+        clearInterval(this.interval);
+        console.clear();
+    }
+};
+
+let filepath = program.filepath ? program.filepath : prompt('Path is required. Type the filepath: ');
+let filename = program.filename ? program.filename + '.css' : 'style.css';
+
+
+loading.start();
+let file = fs.createWriteStream(filepath + filename);
+
+file.on('finish', () => { 
+    loading.stop();
+    console.log('Finished!');
+});
+file.on('error', () => {
+    loading.stop();
+    console.log('err');
+});
+
+for (let i = 1; i <= times; i++) file.write(`.class${i} {background: 'red'};${ i === times ? '' : '\n'}`);
+file.end();
